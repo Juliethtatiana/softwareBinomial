81dd9137fa890ff297d5289fa51a26194fe85caf
JulieJulie <43497453+Juliethtatiana@users.noreply.github.com>
2020-08-19 22:37:33 -0500

    segundo commit

--- interfazBinomial.py
+++ interfazBinomial.py
@@ -14,19 +14,15 @@ class VentanaEjemplo:
         self.etiqueta.pack()
         self.p= Spinbox(master, from_=0.1, to=10)
         self.p.pack()
-        self.etiqueta = Label(master, text="Maximo valor de x:")
-        self.etiqueta.pack()
-        self.x= Spinbox(master, from_=0.01, to=1)
-        self.x.pack()
         self.botonSaludo = Button(master, text="Graficar", command=self.Graficar)
         self.botonSaludo.pack()
         self.botonCerrar = Button(master, text="Cerrar", command=master.quit)
         self.botonCerrar.pack()
     def Graficar(self):
-        N, P = np.int(self.n.get()), np.float(self.p.get()) # parametros de forma 
+        N, P = np.float(self.n.get()), np.float(self.p.get()) # parametros de forma 
         binomial = stats.binom(N, P) # Distribución
         X = np.arange(binomial.ppf(0.01),
-        binomial.ppf(np.float(self.x.get())))
+        binomial.ppf(0.99))
         fmp = binomial.pmf(X) # Función de Masa de Probabilidad
         plt.plot(X, fmp, '--')
         plt.vlines(X, 0, fmp, colors='b', lw=5, alpha=0.5)
